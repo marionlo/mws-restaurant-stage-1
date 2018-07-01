d9f185a315e1c1547ab2fd0bac69406a0f85f9d7
Marion-Lotito <marion.lotito@gmail.com>
2018-07-01 14:58:17 +0200

    feat: add Mapbox API key

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoibWFyaW9ubG8iLCJhIjoiY2pqMnQ4YW03MHl4aTNwbW5ubjBvcTV2cyJ9.yHFxNVKv0LAX4D-f15D4iA',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
